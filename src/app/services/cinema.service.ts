9f19d208a4c032ab41ebb4b26501cc89be1ae4dd
Logesh T <53284256+Logesh18@users.noreply.github.com>
2022-01-08 09:37:15 +0530

    Update cinema.service.ts

--- src/app/services/cinema.service.ts
+++ src/app/services/cinema.service.ts
@@ -11,7 +11,7 @@ export class CinemaService {
   handleError:any;
 
   getMovies(q:string):any{
-    return this.http.get("https://api.themoviedb.org/3/discover/movie?api_key=88c54b4c436e531f2f1b43a7dea9add7&language=en-US&sort_by="+q+".desc&page=1&primary_release_date.gte=2019-01-01&primary_release_date.lte=2021-11-30&vote_average.gte=6");
+    return this.http.get("https://api.themoviedb.org/3/discover/movie?api_key=apikey&language=en-US&sort_by="+q+".desc&page=1&primary_release_date.gte=2019-01-01&primary_release_date.lte=2021-11-30&vote_average.gte=6");
   }
 
       
